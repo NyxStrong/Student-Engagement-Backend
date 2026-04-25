1bf8901c0c221b8f71a5eb4d1d814955e6bdbbb2
NyxStrong <ns0995344@otc.edu>
2026-04-25 06:18:17 -0500

    cors allow all

--- Sources/STB/configure.swift
+++ Sources/STB/configure.swift
@@ -8,6 +8,15 @@ public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
+    let corsConfiguration = CORSMiddleware.Configuration(
+        allowedOrigin: .all,
+        allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH],
+        allowedHeaders: [.accept, .authorization, .contentType, .origin, .xRequestedWith]
+    )
+    
+    // 2. Register middleware at the beginning of the chain
+    app.middleware.use(CORSMiddleware(configuration: corsConfiguration), at: .beginning)
+
     app.http.server.configuration.hostname = "0.0.0.0"
     app.http.server.configuration.port = 8080
 
